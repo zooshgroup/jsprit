5fbce4c3417e5f1c607c1a0b7b6262dcfef8b669
Bálint Száray <6738847+Szbuli@users.noreply.github.com>
2019-05-15 13:13:41 +0200

    reformat

--- jsprit-core/src/main/java/com/graphhopper/jsprit/core/util/ActivityTimeTracker.java
+++ jsprit-core/src/main/java/com/graphhopper/jsprit/core/util/ActivityTimeTracker.java
@@ -25,94 +25,95 @@ import com.graphhopper.jsprit.core.problem.solution.route.activity.TourActivity;
 
 public class ActivityTimeTracker implements ActivityVisitor {
 
-    public static enum ActivityPolicy {
+	public static enum ActivityPolicy {
 
-        AS_SOON_AS_TIME_WINDOW_OPENS, AS_SOON_AS_ARRIVED
+		AS_SOON_AS_TIME_WINDOW_OPENS, AS_SOON_AS_ARRIVED
 
-    }
+	}
 
-    private final ForwardTransportTime transportTime;
+	private final ForwardTransportTime transportTime;
 
-    private final VehicleRoutingActivityCosts activityCosts;
+	private final VehicleRoutingActivityCosts activityCosts;
 
-    private TourActivity prevAct = null;
+	private TourActivity prevAct = null;
 
-    private double startAtPrevAct;
+	private double startAtPrevAct;
 
-    private VehicleRoute route;
+	private VehicleRoute route;
 
-    private boolean beginFirst = false;
+	private boolean beginFirst = false;
 
-    private double actArrTime;
+	private double actArrTime;
 
-    private double actEndTime;
+	private double actEndTime;
 
-    private ActivityPolicy activityPolicy = ActivityPolicy.AS_SOON_AS_TIME_WINDOW_OPENS;
+	private ActivityPolicy activityPolicy = ActivityPolicy.AS_SOON_AS_TIME_WINDOW_OPENS;
 
-    public ActivityTimeTracker(ForwardTransportTime transportTime, VehicleRoutingActivityCosts activityCosts) {
-        super();
-        this.transportTime = transportTime;
-        this.activityCosts = activityCosts;
-    }
+	public ActivityTimeTracker(ForwardTransportTime transportTime, VehicleRoutingActivityCosts activityCosts) {
+		super();
+		this.transportTime = transportTime;
+		this.activityCosts = activityCosts;
+	}
 
-    public ActivityTimeTracker(ForwardTransportTime transportTime, ActivityPolicy activityPolicy, VehicleRoutingActivityCosts activityCosts) {
-        super();
-        this.transportTime = transportTime;
-        this.activityPolicy = activityPolicy;
-        this.activityCosts = activityCosts;
-    }
+	public ActivityTimeTracker(ForwardTransportTime transportTime, ActivityPolicy activityPolicy, VehicleRoutingActivityCosts activityCosts) {
+		super();
+		this.transportTime = transportTime;
+		this.activityPolicy = activityPolicy;
+		this.activityCosts = activityCosts;
+	}
 
-    public double getActArrTime() {
-        return actArrTime;
-    }
+	public double getActArrTime() {
+		return actArrTime;
+	}
 
-    public double getActEndTime() {
-        return actEndTime;
-    }
+	public double getActEndTime() {
+		return actEndTime;
+	}
 
-    @Override
-    public void begin(VehicleRoute route) {
-        prevAct = route.getStart();
-        startAtPrevAct = prevAct.getEndTime();
-        actEndTime = startAtPrevAct;
-        this.route = route;
-        beginFirst = true;
-    }
+	@Override
+	public void begin(VehicleRoute route) {
+		prevAct = route.getStart();
+		startAtPrevAct = prevAct.getEndTime();
+		actEndTime = startAtPrevAct;
+		this.route = route;
+		beginFirst = true;
+	}
 
-    @Override
-    public void visit(TourActivity activity) {
-        if (!beginFirst) throw new IllegalStateException("never called begin. this however is essential here");
-        double transportTime = this.transportTime.getTransportTime(prevAct.getLocation(), activity.getLocation(), startAtPrevAct, route.getDriver(), route.getVehicle());
-        double arrivalTimeAtCurrAct = startAtPrevAct + transportTime;
+	@Override
+	public void visit(TourActivity activity) {
+		if (!beginFirst)
+			throw new IllegalStateException("never called begin. this however is essential here");
+		double transportTime = this.transportTime.getTransportTime(prevAct.getLocation(), activity.getLocation(), startAtPrevAct, route.getDriver(), route.getVehicle());
+		double arrivalTimeAtCurrAct = startAtPrevAct + transportTime;
 
-        actArrTime = arrivalTimeAtCurrAct;
-        double operationStartTime;
+		actArrTime = arrivalTimeAtCurrAct;
+		double operationStartTime;
 
-        if (activityPolicy.equals(ActivityPolicy.AS_SOON_AS_TIME_WINDOW_OPENS)) {
-            operationStartTime = Math.max(activity.getTheoreticalEarliestOperationStartTime(), arrivalTimeAtCurrAct);
-        } else if (activityPolicy.equals(ActivityPolicy.AS_SOON_AS_ARRIVED)) {
-            operationStartTime = actArrTime;
-        } else operationStartTime = actArrTime;
+		if (activityPolicy.equals(ActivityPolicy.AS_SOON_AS_TIME_WINDOW_OPENS)) {
+			operationStartTime = Math.max(activity.getTheoreticalEarliestOperationStartTime(), arrivalTimeAtCurrAct);
+		} else if (activityPolicy.equals(ActivityPolicy.AS_SOON_AS_ARRIVED)) {
+			operationStartTime = actArrTime;
+		} else
+			operationStartTime = actArrTime;
 
-        double operationEndTime = operationStartTime + activityCosts.getActivityDuration(activity,actArrTime,route.getDriver(),route.getVehicle());
+		double operationEndTime = operationStartTime + activityCosts.getActivityDuration(activity, actArrTime, route.getDriver(), route.getVehicle());
 
-        actEndTime = operationEndTime;
+		actEndTime = operationEndTime;
 
-        prevAct = activity;
-        startAtPrevAct = operationEndTime;
+		prevAct = activity;
+		startAtPrevAct = operationEndTime;
 
-    }
+	}
 
-    @Override
-    public void finish() {
-        double transportTime = this.transportTime.getTransportTime(prevAct.getLocation(), route.getEnd().getLocation(), startAtPrevAct, route.getDriver(), route.getVehicle());
-        double arrivalTimeAtCurrAct = startAtPrevAct + transportTime;
+	@Override
+	public void finish() {
+		double transportTime = this.transportTime.getTransportTime(prevAct.getLocation(), route.getEnd().getLocation(), startAtPrevAct, route.getDriver(), route.getVehicle());
+		double arrivalTimeAtCurrAct = startAtPrevAct + transportTime;
 
-        actArrTime = arrivalTimeAtCurrAct;
-        actEndTime = arrivalTimeAtCurrAct;
+		actArrTime = arrivalTimeAtCurrAct;
+		actEndTime = arrivalTimeAtCurrAct;
 
-        beginFirst = false;
-    }
-
+		beginFirst = false;
+	}
 
 }
